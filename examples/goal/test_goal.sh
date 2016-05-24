33608fc02f2c85338b750e14bedcef898e187718
Jacob Menashe <jmenashe@cs.utexas.edu>
2016-05-23 22:14:16 -0500

    Added goal training/testing scripts.

---
+++ examples/goal/test_goal.sh
@@ -0,0 +1,1 @@
+build/tools/caffe test --model=examples/goal/goal_train_test.prototxt --weights=examples/goal/goal_iter_1000.caffemodel -gpu 0 -iterations 100
---
+++ examples/goal/train_goal.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+./build/tools/caffe train --solver=examples/goal/goal_solver.prototxt
