26d95ccec6352770bb681f4f914195d20e56b627
Jacob Menashe <jmenashe@cs.utexas.edu>
2016-06-20 06:41:32 -0500

    Retrained the 1-level ball detector, added a 2-level ball detector.

---
+++ examples/ball2/test.sh
@@ -0,0 +1,1 @@
+build/tools/caffe test --model=examples/ball/ball_train_test.prototxt --weights=examples/ball/ball_iter_46.caffemodel -gpu 0 -iterations 30
---
+++ examples/ball2/train.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+export GLOG_minloglevel=0
+./build/tools/caffe train --solver=examples/ball2/ball_solver.prototxt
